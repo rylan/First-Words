9fe6bdd0019d9b820e3aa2e5f88da52b9e7198c0
Rylan Cottrell <rylan@icottrell.com>
2011-02-28 21:22:43 -0700

    modified for definition

--- src/FirstWords.js
+++ src/FirstWords.js
@@ -10,8 +10,8 @@ enyo.kind({
 			flex:2,	
 			layoutKind: "HFlexLayout", wideWidth: 800, components: [
 				{kind: "com.iCottrell.WordList", name: "wordlist", flex: 1, onclick: "onChange"},
-	    		{kind: enyo.VFlexBox, flex: 3, components: [
-					{kind: "com.iCottrell.Photos", name:"photos", flex:3},
+	    		{kind: enyo.VFlexBox, flex: 3, style:"border-left: 1px solid silver;", components: [
+					{kind: "com.iCottrell.Photos", name:"photos", flex:2},
 					{kind: "com.iCottrell.WordDetails", name: "details", flex:1},
 					
 				]}
@@ -19,8 +19,6 @@ enyo.kind({
 		], 
 		onChange: function(inSender, inEvent){
 			this.$.photos.searchBtnClick(this.$.wordlist.data[inEvent.rowIndex]);
-			this.$.details.updateDefinition(this.$.wordlist.data[inEvent.rowIndex]);
-			this.$.photos.refresh();
-			this.$.details.refresh();
+			this.$.details.updateDefinition(this.$.wordlist.data[inEvent.rowIndex].word);
 		}	
 });
