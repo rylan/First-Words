1cc8d855e8273103e3c80ee2d4f99eda47bc46d9
Rylan Cottrell <rylan@icottrell.com>
2011-05-09 15:45:46 -0600

    minor fixes

--- src/FirstWordsAbout.js
+++ src/FirstWordsAbout.js
@@ -2,12 +2,12 @@ enyo.kind({
 	name: "com.iCottrell.FirstWordsAbout",
 	kind: enyo.Popup, 
 	layoutKind: "VFlexLayout",
-	width: "85%", style: "overflow: hidden",
+	style: "overflow: hidden",
 	components: [
 		{kind: "HFlexBox", components:[
 			{kind: "Image", src:"img/icon.png",  className:"aboutimg"}, 
 			{kind: "VFlexBox", flex:3, components:[
-				{content: "First Wors", className:"abouth1"},
+				{content: "First Words", className:"abouth1"},
 				{content: "Version 1.0.0", className: "aboutver"},
 				{content: "Developed by iCottrell.com", className:"abouttxt"},
 				{content: "Direct all inquries to dev@icottrell.com", className:"abouttxt"}
