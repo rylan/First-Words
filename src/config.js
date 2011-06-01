9dd512750ac83bec7c255ae4d60365e0b55e0a95
Rylan Cottrell <rylan@icottrell.com>
2011-06-01 00:10:22 -0600

    Added lic info into source files

--- src/config.js
+++ src/config.js
@@ -1,3 +1,14 @@
+/******************************************************************************* *
+* Copyright (c) 2010 Rylan Cottrell. iCottrell.com. Calgary, Alberta, Canada.
+* http://www.icottrell.com.  All rights reserved.
+* This implementation is not under an open license of any form at this time. People may
+* utilize it only when we have explicitly authorized them to do so and only for purposes
+* that we have explicitly agreed to. We retain all rights to it. These restrictions are
+* likely to be eased in future. For now, if you wish to use the project for any purpose
+* that we have not already agreed to, please talk to us about it: we are always happy to
+* collaborate.
+******************************************************************************/
+
 enyo.kind({
 	kind:"Control",
 	name: "com.iCottrell.config",
